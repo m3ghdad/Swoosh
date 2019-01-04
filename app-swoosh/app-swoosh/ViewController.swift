808cab7d63d9c879125978c35c54d0c0d66d7ef2
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2019-01-03 22:47:42 -0800

    adds UI for first screen, created a class for custom button

---
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Meghdad Abbaszadegan on 1/3/19.
+//  Copyright © 2019 Meghdad Abbaszadegan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ app-swoosh/app-swoosh/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh
+//
+//  Created by Meghdad Abbaszadegan on 1/3/19.
+//  Copyright © 2019 Meghdad Abbaszadegan. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
